d3edea2260d7fda3b4ce26d46a22a769bfc191d2
starling-king <jigal6518@gmail.com>
2026-06-10 18:00:14 +0530

    fix: update import path for apiClient in admin and contact message services

--- src/Services/admin_users.Services.js
+++ src/Services/admin_users.Services.js
@@ -1,4 +1,4 @@
-import apiClient from "@/config/apiClient";
+import apiClient from "../config/apiClient";
 
 class AdminServices {
 
---
+++ src/Services/contact_messages.Services.js
@@ -0,0 +1,39 @@
+import apiClient from "../config/apiClient";
+
+class ContactMessageService {
+
+    async SavetheDataOfForm({ name, email, message, username }) {
+        try {
+            const payload = {
+                name: name,
+                email: email,
+                message: message
+            };
+            const response = await apiClient.post(`/admin/saveMessage/${username}`, payload);
+            return response.data;
+
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async DiscoverMessage({}) {
+        try {
+            // const payload = {
+            //     name: name,
+            //     email: email,
+            //     message: message
+            // };
+            const response = await apiClient.post('/admin/DiscoverMessage');
+            return response.data;
+
+        } catch (error) {
+            throw error;
+        }
+    }
+
+}
+
+const contactMessageService = new ContactMessageService();
+
+export default contactMessageService;
