9f26bbb1e537eae7ee8d1fc7bc46f1ba96ab6871
starling-king <jigal6518@gmail.com>
2026-06-10 18:27:14 +0530

    add: implement ProjectImagesServices for handling image uploads and user registration

---
+++ src/Services/project_images.Services.js
@@ -0,0 +1,42 @@
+import apiClient from "../config/apiClient";
+
+class ProjectImagesServices {
+
+    async CreateImageCollectionOfProject({ projectId }) {
+        try {
+            const formData = new FormData();
+
+            for (let i = 0; i < imageFiles.length; i++) {
+                formData.append('images', imageFiles[i]);
+            }
+
+            const response = await apiClient.post(`/admin/projects/${projectId}/images`, formData);
+            return response.data;
+
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async registerUser({ projectId, imageId }) {
+        try {
+            // const payload = {
+            //     username: name,
+            //     email: email,
+            //     passwordHash: password
+            // };
+            const response = await apiClient.post(`/admin/projects/${projectId}/Deleteimage/${imageId}`);
+            return response.data;
+
+        } catch (error) {
+            throw error;
+        }
+    }
+
+}
+
+
+
+const projectImagesServices = new ProjectImagesServices();
+
+export default projectImagesServices;
