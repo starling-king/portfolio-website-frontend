b907313bb077758cbc40650da090b0abe8c01bd4
starling-king <jigal6518@gmail.com>
2026-06-22 17:36:07 +0530

    component maping

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,43 @@
+// Layout & UI Components
 import Header from "./Header/Header.jsx";
 import Footer from "./footer/Footer.jsx";
 import LogoutBtn from "./Header/LogoutBtn.jsx";
 import Container from "./container/Container.jsx";
 
-export{
+// Public Visitor Components
+import ProjectCard from "./ProjectCard.jsx";
+import HeroSection from "./HeroSection.jsx";
+import SkillsSection from "./SkillsSection.jsx";
+import ImageGallery from "./ImageGallery.jsx";
+import Loader from "./Loader.jsx";
+
+// Protected Admin Components
+import AdminSidebar from "./AdminSidebar.jsx";
+import AdminProjectList from "./AdminProjectList.jsx";
+import ProjectEditorForm from "./ProjectEditorForm.jsx";
+import ImageUpload from "./ImageUpload.jsx";
+import ContentManager from "./ContentManager.jsx";
+import MessageInbox from "./MessageInbox.jsx";
+
+export {
+  // Layout & UI
   Header,
   Footer,
   LogoutBtn,
-  Container
-}
+  Container,
+  
+  // Public
+  ProjectCard,
+  HeroSection,
+  SkillsSection,
+  ImageGallery,
+  Loader,
+  
+  // Admin
+  AdminSidebar,
+  AdminProjectList,
+  ProjectEditorForm,
+  ImageUpload,
+  ContentManager,
+  MessageInbox
+};
