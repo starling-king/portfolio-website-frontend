92424bf1cba6525a5a7659cabb74baefa4b22988
starling-king <jigal6518@gmail.com>
2026-07-01 15:04:04 +0530

    bug fix

--- src/config/apiClient.js
+++ src/config/apiClient.js
@@ -6,6 +6,9 @@ const apiClient = axios.create({
     timeout: 30000,
     headers: {
         'Content-Type': 'application/json',
+        'Cache-Control': 'no-cache, no-store, must-revalidate',
+        'Pragma': 'no-cache',
+        'Expires': '0'
     },
     withCredentials: true
 });
